5b4151f9ca71eea8eb750cc1f896afd213629baa
EduardoCostadaSilva <eduardocosta5556@gmail.com>
2024-06-03 15:24:17 -0300

    ok

--- JoyPetz/src/navigation/AppNavigator.js
+++ JoyPetz/src/navigation/AppNavigator.js
@@ -1,11 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import ContatoScreen from "../screens/ContatoScreen/ContatoScreen";
 import LoginScreen from "../screens/LoginScreen/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import PublicarScreen from "../screens/PublicarScreen/PublicarScreen";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import ContatoScreen from "../screens/ContatoScreen/ContatoScreen";
 import QSScreen from "../screens/QuemSomosScreen/QSScreen";
 
 const Stack = createStackNavigator();
@@ -15,19 +15,20 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="JoyPetz">
+        <Stack.Screen name="Joypetz">
           {() => (
             <Drawer.Navigator>
+              <Drawer.Screen name="Login" component={LoginScreen} options={{ headerShown: false,}}/>
+              <Drawer.Screen name="Register" component={RegisterScreen} options={{ headerShown: false,}}/>
               <Drawer.Screen name="Home" component={HomeScreen} />
               <Drawer.Screen name="Publicar" component={PublicarScreen} />
               <Drawer.Screen name="Contato" component={ContatoScreen} />
               <Drawer.Screen name="Quem Somos" component={QSScreen} />
-              <Drawer.Screen name="Login" component={LoginScreen} />
-              <Drawer.Screen name="Register" component={RegisterScreen} />
             </Drawer.Navigator>
           )}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
