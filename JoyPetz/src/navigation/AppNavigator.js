b2cb646a2c60f3054f016f78b0f620cbe5b79975
EduardoCostadaSilva <eduardocosta5556@gmail.com>
2024-06-07 14:47:02 -0300

    ok

--- JoyPetz/src/navigation/AppNavigator.js
+++ JoyPetz/src/navigation/AppNavigator.js
@@ -1,11 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import ContatoScreen from "../screens/ContatoScreen/ContatoScreen";
-import LoginScreen from "../screens/LoginScreen/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import PublicarScreen from "../screens/PublicarScreen/PublicarScreen";
 import QSScreen from "../screens/QuemSomosScreen/QSScreen";
+import LoginScreen from "../screens/LoginScreen/LoginScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -13,7 +13,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer> 
             <Drawer.Navigator>
-              <Drawer.Screen name="Login" component={LoginScreen} options={{ headerShown: false,}}/>
+              <Drawer.Screen name="Login" component={LoginScreen} options={{ headerShown: false, drawerLabel: () => null, title: null,}}/>
               <Drawer.Screen name="Register" component={RegisterScreen} options={{ headerShown: false,}}/>
               <Drawer.Screen name="Home" component={HomeScreen} />
               <Drawer.Screen name="Publicar" component={PublicarScreen} />
--- JoyPetz/src/screens/LoginScreen/LoginScreen.jsx
+++ JoyPetz/src/screens/LoginScreen/LoginScreen.jsx
@@ -1,29 +1,12 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import {TextInput } from "react-native-paper";
+import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
-export default function LoginScreen({ navigation }) {
-  function onPressLogin() {
-    navigation.navigate("Home");
-  }
 
-  return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-
-      <TextInput label="Email" />
-      <TextInput label="Senha" />
-
-      <Button onPress={onPressLogin}>Entrar</Button>
-      <Button mode="contained">Registrar-se</Button>
-
-    </View>
-  );
+export default function LoginScreen({navigation}) {
+    return(
+        <View>
+            <Text>Bom dia</Text>
+            <Button onPress={() => navigation.navigate('Home')}>logar</Button>
+        </View>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
